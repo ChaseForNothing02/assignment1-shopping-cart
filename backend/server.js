b4e5d923771c76e2d6f28273f311914344da2e34
ChaseForNothing02 <litianqi30@gmail.com>
2026-03-30 12:08:42 +1100

    testing for database creation

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Backend server is running");
+});
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/shopping-cart")
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
